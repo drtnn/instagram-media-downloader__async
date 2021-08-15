49ca3df2a04245d1a1fc2cd31e6c21eb9068a680
drtnn <8david@inbox.ru>
2021-08-15 21:11:12 +0300

    Creating new request for analytic to database

--- handlers/users/subscriber/instagram_highlight.py
+++ handlers/users/subscriber/instagram_highlight.py
@@ -1,5 +1,6 @@
 from aiogram.types import Message, InlineQuery
 from loader import dp, bot, upload_client
+from utils.db_api.database import Requests
 from utils.instagram import InstagramHighlight
 
 
@@ -8,6 +9,7 @@ async def instagram_highlight_handler(message: Message):
     post = InstagramHighlight(message.text)
     await post.start()
     await post.send_to(bot=bot, upload_client=upload_client, chat_id=message.chat.id)
+    await Requests.add(user_id=message.from_user.id, content_type='h')
 
 
 @dp.inline_handler(is_subscriber=True, instagram_inline_highlight=True, state='*')
@@ -15,3 +17,4 @@ async def instagram_highlight_inline_handler(query: InlineQuery):
     highlight = InstagramHighlight(query.query.strip())
     await highlight.start()
     await highlight.inline_to(query=query)
+    await Requests.add(user_id=query.from_user.id, content_type='h')
--- handlers/users/subscriber/instagram_post.py
+++ handlers/users/subscriber/instagram_post.py
@@ -1,5 +1,6 @@
 from aiogram.types import Message, InlineQuery
 from loader import dp, bot, upload_client
+from utils.db_api.database import Requests
 from utils.instagram import InstagramPost
 
 
@@ -8,6 +9,7 @@ async def instagram_post_handler(message: Message):
     post = InstagramPost(message.text)
     await post.start()
     await post.send_to(bot=bot, upload_client=upload_client, chat_id=message.chat.id)
+    await Requests.add(user_id=message.from_user.id, content_type='p')
 
 
 @dp.inline_handler(is_subscriber=True, instagram_inline_post=True, state='*')
@@ -15,3 +17,4 @@ async def instagram_post_inline_handler(query: InlineQuery):
     post = InstagramPost(query.query.strip())
     await post.start()
     await post.inline_to(query=query)
+    await Requests.add(user_id=query.from_user.id, content_type='p')
